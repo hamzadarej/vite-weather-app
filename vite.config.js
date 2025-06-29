8963e602fa07ea751bea4fab816d489b2e3f47e3
Hamza <hamza.darej@check24.de>
2025-06-29 16:11:54 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_URL || '/vite-weather-app'
+  base: process.env.VITE_BASE_URL || '/vite-weather-app'
 })
